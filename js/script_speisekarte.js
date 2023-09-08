aad9e877332130088675f4e175fbcc51f77e864c
janslz <143496056+Janslz@users.noreply.github.com>
2023-09-08 23:21:09 +0200

    speisekarte angefangen beilagen fehlen noch

---
+++ js/script_speisekarte.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var menuItems = document.querySelectorAll('.menu-item');
+    var menuImage = document.getElementById('menu-image');
+
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener('mouseover', function () {
+            var imageName = menuItem.getAttribute('data-image');
+            menuImage.src = imageName;
+            menuImage.style.display = 'block';
+        });
+
+        menuItem.addEventListener('mouseout', function () {
+            menuImage.style.display = 'none';
+        });
+    });
+});
